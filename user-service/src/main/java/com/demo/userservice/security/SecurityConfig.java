4ffb8f52280b42e1b39df94f4dd3e9fa4fa959ea
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-06-10 03:12:57 +0300

    add SecurityConfig

---
+++ user-service/src/main/java/com/demo/userservice/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.demo.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> {
+            request.requestMatchers("/**").permitAll();
+            request.requestMatchers("/h2-console/**").permitAll();
+            request.requestMatchers("api/v1/users/**").permitAll();
+        });
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));//for h2-db
+        return http.build();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
