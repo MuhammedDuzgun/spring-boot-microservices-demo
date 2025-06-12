80be7fe6222b542304e4b3ea8ef327a71291dd97
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-06-13 02:45:52 +0300

    format code

--- user-service/src/main/java/com/demo/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/demo/userservice/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 public class SecurityConfig {
